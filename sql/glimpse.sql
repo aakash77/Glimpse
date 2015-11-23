d086bebeaf76ce3522efceb0e322915293163b08
Aakash Mangal <aakash.mangal77@gmail.com>
2015-11-22 18:26:47 -0800

    updated sql

--- sql/glimpse.sql
+++ sql/glimpse.sql
@@ -23,13 +23,13 @@ DROP TABLE IF EXISTS `invitations`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `invitations` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `invitation_id` bigint(20) NOT NULL AUTO_INCREMENT,
   `sender` varchar(100) NOT NULL,
   `project_id` bigint(20) NOT NULL,
   `receiver` varchar(100) NOT NULL,
   `status` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`)
+  PRIMARY KEY (`invitation_id`),
+  UNIQUE KEY `id_UNIQUE` (`invitation_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -37,10 +37,8 @@ CREATE TABLE `invitations` (
 -- Dumping data for table `invitations`
 --
 
-LOCK TABLES `invitations` WRITE;
 /*!40000 ALTER TABLE `invitations` DISABLE KEYS */;
 /*!40000 ALTER TABLE `invitations` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `project`
@@ -50,12 +48,12 @@ DROP TABLE IF EXISTS `project`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `project` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `project_id` bigint(20) NOT NULL AUTO_INCREMENT,
   `title` bigint(100) NOT NULL,
   `description` varchar(100) DEFAULT NULL,
   `owner` bigint(20) DEFAULT NULL,
   `state` varchar(100) DEFAULT NULL,
-  UNIQUE KEY `id_UNIQUE` (`id`)
+  UNIQUE KEY `id_UNIQUE` (`project_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -63,10 +61,8 @@ CREATE TABLE `project` (
 -- Dumping data for table `project`
 --
 
-LOCK TABLES `project` WRITE;
 /*!40000 ALTER TABLE `project` DISABLE KEYS */;
 /*!40000 ALTER TABLE `project` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `project_state`
@@ -76,9 +72,9 @@ DROP TABLE IF EXISTS `project_state`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `project_state` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `project_state_id` bigint(20) NOT NULL AUTO_INCREMENT,
   `value` varchar(45) NOT NULL,
-  UNIQUE KEY `id_UNIQUE` (`id`),
+  UNIQUE KEY `id_UNIQUE` (`project_state_id`),
   UNIQUE KEY `value_UNIQUE` (`value`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -87,11 +83,9 @@ CREATE TABLE `project_state` (
 -- Dumping data for table `project_state`
 --
 
-LOCK TABLES `project_state` WRITE;
 /*!40000 ALTER TABLE `project_state` DISABLE KEYS */;
 INSERT INTO `project_state` VALUES (3,'cancelled'),(4,'completed'),(2,'ongoing'),(1,'planning');
 /*!40000 ALTER TABLE `project_state` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `task`
@@ -101,7 +95,7 @@ DROP TABLE IF EXISTS `task`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `task` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `task_id` bigint(20) NOT NULL AUTO_INCREMENT,
   `project_id` varchar(100) DEFAULT NULL,
   `title` varchar(100) DEFAULT NULL,
   `description` varchar(100) DEFAULT NULL,
@@ -109,7 +103,7 @@ CREATE TABLE `task` (
   `state` varchar(100) DEFAULT NULL,
   `estimate` int(20) DEFAULT NULL,
   `actual` int(20) DEFAULT NULL,
-  UNIQUE KEY `id_UNIQUE` (`id`)
+  UNIQUE KEY `id_UNIQUE` (`task_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -117,10 +111,8 @@ CREATE TABLE `task` (
 -- Dumping data for table `task`
 --
 
-LOCK TABLES `task` WRITE;
 /*!40000 ALTER TABLE `task` DISABLE KEYS */;
 /*!40000 ALTER TABLE `task` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `task_state`
@@ -130,10 +122,10 @@ DROP TABLE IF EXISTS `task_state`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `task_state` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `task_state_id` bigint(20) NOT NULL AUTO_INCREMENT,
   `value` varchar(45) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`),
+  PRIMARY KEY (`task_state_id`),
+  UNIQUE KEY `id_UNIQUE` (`task_state_id`),
   UNIQUE KEY `value_UNIQUE` (`value`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -142,11 +134,9 @@ CREATE TABLE `task_state` (
 -- Dumping data for table `task_state`
 --
 
-LOCK TABLES `task_state` WRITE;
 /*!40000 ALTER TABLE `task_state` DISABLE KEYS */;
 INSERT INTO `task_state` VALUES (2,'assigned'),(5,'cancelled'),(4,'finished'),(1,'new'),(3,'started');
 /*!40000 ALTER TABLE `task_state` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `team`
@@ -167,10 +157,8 @@ CREATE TABLE `team` (
 -- Dumping data for table `team`
 --
 
-LOCK TABLES `team` WRITE;
 /*!40000 ALTER TABLE `team` DISABLE KEYS */;
 /*!40000 ALTER TABLE `team` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `user`
@@ -183,7 +171,9 @@ CREATE TABLE `user` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `email` varchar(100) NOT NULL,
   `name` varchar(100) DEFAULT NULL,
-  UNIQUE KEY `id_UNIQUE` (`id`)
+  `password` varchar(1000) NOT NULL,
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  UNIQUE KEY `email_UNIQUE` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -191,10 +181,8 @@ CREATE TABLE `user` (
 -- Dumping data for table `user`
 --
 
-LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -205,4 +193,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-11-20  1:56:13
+-- Dump completed on 2015-11-22 17:11:41
